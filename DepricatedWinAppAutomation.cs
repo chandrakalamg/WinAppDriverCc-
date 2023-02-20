f564813c1a754a7f32af0ab6346cd35b98e90328
Chandrakala Gangadharaiah <chandrakala.gangadharaiah@btl.com>
2023-02-20 21:22:58 +0000

    Not complated - Appium WinAppdriver

---
+++ DepricatedWinAppAutomation.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace WinAppDriverSeleniumAutomation
+{
+    class DepricatedWinAppAutomation
+    {
+
+        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string CalculatorAppId = "C:\\Users\\ChandrakalaG\\AppData\\Local\\SourceTree\\SourceTree.exe";
+
+        protected static WindowsDriver<WindowsElement> session;
+
+        public static void Setup(TestContext context)
+        {
+            // Launch Calculator application if it is not yet launched
+            if (session == null)
+            {
+                /*// Create a new session to bring up an instance of the Calculator application
+                
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app", "C:\\Users\\ChandrakalaG\\AppData\\Local\\SourceTree\\SourceTree.exe");
+                appCapabilities.SetCapability("deviceName", "WindowsPC");
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+                Assert.IsNotNull(session);
+
+               
+                session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);*/
+            }
+        }
+
+        public static void TearDown()
+        {
+            
+            if (session != null)
+            {
+                session.Quit();
+                session = null;
+            }
+        }
+    }
+}
+
---
+++ SourcetressWinAppAppiumOptions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+
+namespace WinAppDriverSeleniumAutomation
+{
+    class SourcetreeWinAppAutomation
+    {
+        private static WindowsElement sourcetreeResult;
+        protected static WindowsDriver<WindowsElement> sourcetreeSession;
+        static void Main(string[] args)
+        {
+            WindowsDriver<WindowsElement> sourcetreeSession;
+            AppiumOptions desiredCapabilities = new AppiumOptions();
+            desiredCapabilities.AddAdditionalCapability("app", "C:\\Users\\ChandrakalaG\\AppData\\Local\\SourceTree\\SourceTree.exe");
+            sourcetreeSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"), desiredCapabilities);
+            sourcetreeSession.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            sourcetreeResult = sourcetreeSession.FindElementByXPath("//*[@Name='Sourcetree']");
+            Assert.IsNotNull(sourcetreeResult);
+
+
+            Console.WriteLine("click on remote1");
+            sourcetreeSession.FindElementByName("Remote").Click();
+
+            Console.WriteLine("Click Edit accounts");
+            sourcetreeSession.FindElementByName("Edit Accounts...").Click();
+            Console.WriteLine(sourcetreeSession.FindElementByName("Options").Displayed);
+        }
+
+        public static void TearDown()
+        {
+
+            if (sourcetreeSession != null)
+            {
+                sourcetreeSession.Quit();
+                sourcetreeSession = null;
+            }
+        }
+/*
+        //[TestMethod]
+        //public void remote()
+        //{
+
+        //    Console.WriteLine("click on remote1");
+        //    sourcetreeSession.FindElementByName("Remote").Click();
+
+        //}
+        //[TestMethod]
+        //public void EditAccounts()
+        //{
+        //    Console.WriteLine("Click Edit accounts");
+        //    sourcetreeSession.FindElementByName("Edit Accounts...").Click();
+        //    Console.WriteLine(sourcetreeSession.FindElementByName("Options").Displayed);
+
+        //}*/
+
+
+    }
+}
